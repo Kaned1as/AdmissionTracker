5a4612d2321e22f603aae423fd12a6f0955e4ccf
Адонай <adonai@xaker.ru>
2014-07-09 16:57:58 +0400

    - implement google adMob support

--- tracker/src/main/java/com/adonai/admissiontracker/CheckProPreference.java
+++ tracker/src/main/java/com/adonai/admissiontracker/CheckProPreference.java
@@ -10,9 +10,10 @@ import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.util.AttributeSet;
 
-import com.mobclix.android.sdk.MobclixAdView;
-import com.mobclix.android.sdk.MobclixAdViewListener;
-import com.mobclix.android.sdk.MobclixIABRectangleMAdView;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 /**
  * Created by adonai on 02.07.14.
@@ -48,70 +49,8 @@ public class CheckProPreference extends CheckBoxPreference {
                     final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
                     builder.setTitle(R.string.but_excuse);
                     builder.setMessage(R.string.only_pro);
-                    builder.setPositiveButton(R.string.click_ad, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            final AlertDialog.Builder adBuilder = new AlertDialog.Builder(getContext());
-                            final MobclixIABRectangleMAdView adView = new MobclixIABRectangleMAdView(getContext());
-                            adBuilder.setView(adView);
-                            final Dialog advertiseDialog = adBuilder.create();
-                            advertiseDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                                @Override
-                                public void onDismiss(DialogInterface dialog) {
-                                    adView.pause();
-                                }
-                            });
-                            advertiseDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-                                @Override
-                                public void onCancel(DialogInterface dialog) {
-                                    adView.pause();
-                                }
-                            });
-                            adView.addMobclixAdViewListener(new MobclixAdViewListener() {
-
-                                @Override
-                                public void onSuccessfulLoad(MobclixAdView mobclixAdView) {
-
-                                }
-
-                                @Override
-                                public void onFailedLoad(MobclixAdView mobclixAdView, int i) {
-
-                                }
-
-                                @Override
-                                public void onAdClick(MobclixAdView mobclixAdView) {
-                                    if (!CheckProPreference.this.isChecked())
-                                        CheckProPreference.super.onClick();
-
-                                    // extend clicktime for a day
-                                    getEditor().putLong(NetworkService.PREF_CLICKTIME, System.currentTimeMillis()).apply();
-                                    advertiseDialog.dismiss();
-                                }
-
-                                @Override
-                                public boolean onOpenAllocationLoad(MobclixAdView mobclixAdView, int i) {
-                                    return false;
-                                }
-
-                                @Override
-                                public void onCustomAdTouchThrough(MobclixAdView mobclixAdView, String s) {
-
-                                }
-
-                                @Override
-                                public String keywords() {
-                                    return null;
-                                }
-
-                                @Override
-                                public String query() {
-                                    return null;
-                                }
-                            });
-                            advertiseDialog.show();
-                        }
-                    });
+                    //builder.setPositiveButton(R.string.click_ad, new MobClixOnClickListener());
+                    //builder.setPositiveButton(R.string.click_ad, new MobClixOnClickListener());
                     builder.setNeutralButton(R.string.buy_pro, new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
@@ -126,4 +65,103 @@ public class CheckProPreference extends CheckBoxPreference {
             }
         });
     }
+
+    private class MobClixOnClickListener implements DialogInterface.OnClickListener {
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            final AlertDialog.Builder adBuilder = new AlertDialog.Builder(getContext());
+            final AdView adView = new AdView(getContext());
+            adView.setAdUnitId("ca-app-pub-2426537600463724/5449840696");
+            adView.setAdSize(AdSize.MEDIUM_RECTANGLE);
+            AdRequest adRequest = new AdRequest.Builder().build();
+            adView.loadAd(adRequest);
+            adBuilder.setView(adView);
+
+            /*AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)       // Эмулятор
+                .addTestDevice("AC98C820A50B4AD8A2106EDE96FB87D4") // Тестовый телефон Galaxy Nexus
+                .build();*/
+
+            final Dialog advertiseDialog = adBuilder.create();
+            adView.pause();
+            adView.setAdListener(new AdListener() {
+                @Override
+                public void onAdClosed() {
+                    if (!CheckProPreference.this.isChecked())
+                        CheckProPreference.super.onClick();
+                    advertiseDialog.dismiss();
+                }
+
+            });
+            advertiseDialog.show();
+        }
+    }
+
+    /*
+    private class MobClixOnClickListener implements DialogInterface.OnClickListener {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            final AlertDialog.Builder adBuilder = new AlertDialog.Builder(getContext());
+            final MobclixIABRectangleMAdView adView = new MobclixIABRectangleMAdView(getContext());
+            adBuilder.setView(adView);
+            final Dialog advertiseDialog = adBuilder.create();
+            advertiseDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    adView.pause();
+                }
+            });
+            advertiseDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    adView.pause();
+                }
+            });
+            adView.addMobclixAdViewListener(new MobclixAdViewListener() {
+
+                @Override
+                public void onSuccessfulLoad(MobclixAdView mobclixAdView) {
+
+                }
+
+                @Override
+                public void onFailedLoad(MobclixAdView mobclixAdView, int i) {
+
+                }
+
+                @Override
+                public void onAdClick(MobclixAdView mobclixAdView) {
+                    if (!CheckProPreference.this.isChecked())
+                        CheckProPreference.super.onClick();
+
+                    // extend clicktime for a day
+                    getEditor().putLong(NetworkService.PREF_CLICKTIME, System.currentTimeMillis()).apply();
+                    advertiseDialog.dismiss();
+                }
+
+                @Override
+                public boolean onOpenAllocationLoad(MobclixAdView mobclixAdView, int i) {
+                    return false;
+                }
+
+                @Override
+                public void onCustomAdTouchThrough(MobclixAdView mobclixAdView, String s) {
+
+                }
+
+                @Override
+                public String keywords() {
+                    return null;
+                }
+
+                @Override
+                public String query() {
+                    return null;
+                }
+            });
+            advertiseDialog.show();
+        }
+    }
+    */
 }
